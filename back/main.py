397c8fbbacf2807392fd857659e9fdf34bf11b99
chachezone <1209486252@qq.com>
2026-07-01 08:21:38 +0800

    换个端口，与build生产环境重叠

--- back/main.py
+++ back/main.py
@@ -29,4 +29,4 @@ app.include_router(draw)
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run('main:app', host='0.0.0.0', port=8070, reload=True)
+    uvicorn.run('main:app', host='0.0.0.0', port=8010, reload=True)
--- front/vite.config.js
+++ front/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1:8070',
+        target: 'http://127.0.0.1:8010',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
